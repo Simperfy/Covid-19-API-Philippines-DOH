a8b2a55e257bae27b499df585ca3d1a780db840e
Ragkhuza <Dargonhelsing@gmail.com>
2020-07-28 19:30:46 +0800

    log changes

--- src/GoogleDriveApiClient.js
+++ src/GoogleDriveApiClient.js
@@ -45,13 +45,15 @@ class GoogleDriveApi {
       if (this.isServiceKeysValid()) {
         console.log('Service key is valid!');
         console.log('output: ');
+
+        const privateKey = process.env.PRIVATE_KEY.replace(new RegExp('\\\\n', '\g'), '\n');
         this.auth = new GoogleAuth({
           scopes: SCOPES,
         }).fromJSON({
           'type': process.env.TYPE,
           'project_id': process.env.PROJECT_ID,
           'private_key_id': process.env.PRIVATE_KEY_ID,
-          'private_key': process.env.PRIVATE_KEY,
+          'private_key': privateKey,
           'client_email': process.env.CLIENT_EMAIL,
           'client_id': process.env.CLIENT_ID,
         },
