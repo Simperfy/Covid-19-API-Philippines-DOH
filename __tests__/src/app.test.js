aef790128e13993e4c3fd16c28189bf66b94f56b
Ragkhuza <Dargonhelsing@gmail.com>
2020-08-03 05:04:52 +0800

    ADDED: app.js test

---
+++ __tests__/src/app.test.js
@@ -0,0 +1,69 @@
+/* eslint-disable max-len */
+const request = require('supertest');
+console.log = () => {}; // disable logs
+const app = require('../../app');
+
+jest.setTimeout(300000);
+test('GET /api/summary', async () => {
+  const res = await request(app).get('/api/summary');
+
+  expect(res.status).toBe(200);
+  expect(res.body).toHaveProperty('data');
+  expect(res.body.data).toHaveProperty('fatality_rate');
+  expect(res.body.data).toHaveProperty('recovery_rate');
+  expect(res.body).toHaveProperty('last_update');
+});
+
+test('GET /api/get', async () => {
+  const res = await request(app).get('/api/get');
+
+  expect(res.status).toBe(200);
+  expect(res.body).toHaveProperty('data');
+  expect(res.body.data).toHaveLength(10000);
+  expect(res.body).toHaveProperty('last_update');
+  expect(res.body).toHaveProperty('pagination');
+  expect(res.body).toHaveProperty('result_count');
+  expect(res.body.result_count).toBe(10000);
+});
+
+test('GET /api/get?page=1&limit=5000', async () => {
+  const res = await request(app).get('/api/get?page=1&limit=5000');
+
+  expect(res.status).toBe(200);
+  expect(res.body).toHaveProperty('data');
+  expect(res.body.data).toHaveLength(5000);
+  expect(res.body).toHaveProperty('last_update');
+  expect(res.body).toHaveProperty('pagination');
+  expect(res.body).toHaveProperty('result_count');
+  expect(res.body.result_count).toBe(5000);
+});
+
+test('GET /api/get?page=-1&limit=-1', async () => {
+  const res = await request(app).get('/api/get?page=-1&limit=-1');
+
+  expect(res.status).toBe(200);
+  expect(res.body).toHaveProperty('data');
+  expect(res.body.data).toHaveLength(0);
+  expect(res.body).toHaveProperty('last_update');
+  expect(res.body).toHaveProperty('error');
+});
+
+test('GET /api/timeline', async () => {
+  const res = await request(app).get('/api/timeline');
+
+  expect(res.status).toBe(200);
+  expect(res.body).toHaveProperty('data');
+  expect(res.body.data).not.toHaveLength(0);
+  expect(res.body.data[0]).toHaveProperty('cases');
+  expect(res.body.data[0]).toHaveProperty('date');
+  expect(res.body).toHaveProperty('last_update');
+});
+
+test('GET /api/filter/region_res/Region IV-A: CALABARZON', async () => {
+  const res = await request(app).get('/api/filter/region_res/Region IV-A: CALABARZON');
+
+  expect(res.status).toBe(200);
+  expect(res.body).toHaveProperty('data');
+  expect(res.body.data).not.toHaveLength(0);
+  expect(res.body).toHaveProperty('last_update');
+});
