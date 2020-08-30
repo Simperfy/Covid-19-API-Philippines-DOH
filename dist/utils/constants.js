40d3c4b9e68cbb6db5fec1e30410f33146392dc2
Simperfy <28738855+Simperfy@users.noreply.github.com>
2020-08-31 03:35:34 +0800

    REMOVED: compiled js files

--- dist/utils/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.filesToSearch = void 0;
-/* eslint-disable max-len */
-exports.filesToSearch = ['Case Information.csv', 'DOH Data Collect - Daily Report.csv'];
-//# sourceMappingURL=constants.js.map
